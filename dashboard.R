ebd160302904f3ec3203c80ef22936da8a84c7b8
tinaluot2 <luot2@student.unimelb.edu.au>
2020-09-10 17:38:57 +1000

    Update dashboard.R

--- dashboard.R
+++ dashboard.R
@@ -4,9 +4,47 @@ install.packages("shiny", dependencies = TRUE)
 library(shinydashboard)
 library(shiny)
 
-#making a simple UI
-dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+#splitting parts easier readability
+header <- dashboardHeader(title = "COVID-19 Finance Dashboard")
+
+sidebar <- dashboardSidebar(sidebarMenu(
+  sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
+                    label = "Search..."),
+  menuItem("COVID Stocks", tabName = "COVID Stocks", icon = icon("chart-bar", lib = "font-awesome")),
+  menuItem("COVID Government", tabName = "COVID Government", icon = icon("book")),
+  menuItem("COVID Cryptocurrency", tabName = "COVID Cryptocurrency", icon = icon("money")),
+  menuItem("COVID Ecommerce", tabName = "COVID Ecommerce", icon = icon("shopping-cart", lib = "font-awesome")),
+  menuItem("COVID Banking", tabName = "COVID Banking", icon = icon("piggy-bank", lib = "font-awesome"))
+))
+
+body <- dashboardBody(
+  fluidRow(
+    
+    box(
+      title = "Histogram", status = "primary", solidHeader = TRUE,
+      collapsible = TRUE,
+      plotOutput("plot1", height = 250)),
+    
+    box(
+      title = "Parameter Settings", status = "warning",solidHeader = TRUE,
+      "Box content here", br(), "More box content",
+      sliderInput("slider", "Slider input:", 1, 100, 50),
+      textInput("text", "Text input:")
+    )
+  )
 )
+
+interface <- dashboardPage(header, sidebar, body)
+
+
+site <- function(input, output) { 
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(interface, site)
