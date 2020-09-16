f9ce22bacdc562315748a3a6696e13080eb50838
Michelle Tan <michelle.tanujaya18@gmail.com>
2020-09-16 23:26:18 +1000

    adding sub menu for visualisation

--- dashboard.R
+++ dashboard.R
@@ -13,35 +13,59 @@ header <- dashboardHeader(title = "COVID-19 Finance Dashboard")
 sidebar <- dashboardSidebar(sidebarMenu(
   sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
                     label = "Search..."),
-  menuItem("COVID Stocks", tabName = "COVID Stocks", icon = icon("chart-bar", lib = "font-awesome")),
-  menuItem("COVID Government", tabName = "COVID Government", icon = icon("book")),
-  menuItem("COVID Cryptocurrency", tabName = "COVID Cryptocurrency", icon = icon("money")),
-  menuItem("COVID Ecommerce", tabName = "COVID Ecommerce", icon = icon("shopping-cart", lib = "font-awesome")),
-  menuItem("COVID Banking", tabName = "COVID Banking", icon = icon("piggy-bank", lib = "font-awesome"))
+  menuItem("Data Visualisation", tabName = "Data_Visualisation", icon = icon("chart-bar", lib = "font-awesome"),
+           startExpanded = TRUE,
+           menuSubItem("Unemployment Rate", tabName = "Unemployment_Rate")),
+  menuItem("COVID Stocks", tabName = "COVID_Stocks", icon = icon("chart-bar", lib = "font-awesome")),
+  menuItem("COVID Government", tabName = "COVID_Government", icon = icon("book")),
+  menuItem("COVID Cryptocurrency", tabName = "COVID_Cryptocurrency", icon = icon("money")),
+  menuItem("COVID Ecommerce", tabName = "COVID_Ecommerce", icon = icon("shopping-cart", lib = "font-awesome")),
+  menuItem("COVID Banking", tabName = "COVID_Banking", icon = icon("piggy-bank", lib = "font-awesome"))
 ))
 
 body <- dashboardBody(
-  fluidRow(
-    
-    box(
-      title = "Histogram", status = "primary", solidHeader = TRUE,
-      collapsible = TRUE,
-      plotOutput("plot1", height = 250)),
-    
-    box(
-      title = "Parameter Settings", status = "warning",solidHeader = TRUE,
-      "Box content here", br(), "More box content",
-      sliderInput("slider", "Slider input:", 1, 100, 50),
-      textInput("text", "Text input:")
+  tabItems(
+    tabItem("Unemployment_Rate", 
+            fluidPage(
+              fluidRow("Charts"),
+              fluidRow("Description")
+            )),
+    tabItem(tabName = "COVID_Stocks",
+            fluidRow(
+              box(
+                title = "Histogram", status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                plotOutput("plot1", height = 250)
+              ),
+              box(
+                title = "Parameter Settings", status = "warning",solidHeader = TRUE,
+                "Box content here", br(), "More box content",
+                sliderInput("slider", "Slider input:", 1, 100, 50),
+                textInput("text", "Text input:")
+              ),
+              box(
+                title = "Table", status = "warning",solidHeader = TRUE,
+                collapsible = TRUE,
+                dataTableOutput('table')
+              )
+            )
     ),
     
-    box(
-      title = "Table", status = "warning",solidHeader = TRUE,
-      collapsible = TRUE,
-      dataTableOutput('table')
+    tabItem(tabName = "COVID_Government",
+            h2("COVID Government tab content")
+    ),
+    
+    tabItem(tabName = "COVID_Cryptocurrency",
+            h2("COVID Cryptocurrency tab content")
+    ),
+    
+    tabItem(tabName = "COVID_Ecommerce",
+            h2("COVID Ecommerce tab content")
+    ),
+    
+    tabItem(tabName = "COVID_Banking",
+            h2("COVID Banking tab content")
     )
-    
-    
   )
 )
 
@@ -51,7 +75,7 @@ interface <- dashboardPage(header, sidebar, body)
 
 site <- function(input, output) { 
   #import dataset
-  tbl <- read_csv("population.csv")
+  tbl <- read_csv("small_covid_data.csv")
   
   output$table <- renderDataTable(tbl)
   
